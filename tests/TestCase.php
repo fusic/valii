2fcfd1586403544bd990e7faf49579584314494e
tutida <tomo0991@gmail.com>
2019-07-04 11:37:49 +0000

    add doc comment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,13 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [
